65d31db5105d0b1cab65ffe56d5c08777cc8a4a6
aakash55555 <aakashrastogi22@gmail.com>
2020-10-21 14:56:19 -0700

    Updated dataset_generation.py
    Modified the output format and get the output in a file

--- dataset_generation.py
+++ dataset_generation.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import spacy
+import sys
+spacy.prefer_gpu()
 nlp = spacy.load("en_core_web_sm")
 import random
 import re
 
 data = pd.read_csv("balacopa-dev-all.csv")
+count = 0
 
 def select_replace(line, chunk):
     word = random.choice(tuple(chunk))
@@ -34,27 +37,43 @@ for row in data.itertuples(index=True, name='Pandas'):
     # print(chunka2)
 
     line = row.p+" "+row.a1+" "+row.a2
-
     # print(len(chunkp.intersection(chunka1)))
     # print(len(chunka1.intersection(chunka2)))
     # print(len(chunkp.intersection(chunka2)))
     if(len(chunkp.intersection(chunka1))>0):
         line, referring_word, pronoun = select_replace(row.a1, chunka1)
-        print(row.p+" "+line+" "+row.a2)
-        print(referring_word)
-        print(pronoun)
+        sys.stdout = open("model_output.txt", "a")
+        count = count + 1
+        phrase = row.p+" "+line+" "+row.a2
+        output = "Id: " +str(count)+ ", Sentence: "+ phrase+", Option1: "+referring_word+", Option2: " +referring_word+", Answer: "+referring_word  
+        print(output)
+        #print(row.p+" "+line+" "+row.a2)
+        #print(referring_word)
+        #print(pronoun)
+        sys.stdout.close()
         # continue
     if(len(chunka1.intersection(chunka2))>0):
         line, referring_word, pronoun = select_replace(row.a2, chunka2)
-        print(row.p+" "+row.a1+" "+line)
-        print(referring_word)
-        print(pronoun)
+        sys.stdout = open("model_output.txt", "a")
+        count = count + 1
+        phrase = row.p+" "+row.a1+" "+line 
+        output = "Id: " +str(count)+ ", Sentence: "+ phrase+", Option1: "+referring_word+", Option2: " +referring_word+", Answer: "+referring_word  
+        print(output)
+        #print(row.p+" "+row.a1+" "+line)
+        #print(referring_word)
+        #print(pronoun)
+        sys.stdout.close()
         # continue
     if(len(chunkp.intersection(chunka2))>0):
         line, referring_word, pronoun = select_replace(row.a2, chunka2)
-        print(row.p+" "+row.a1+" "+line)
-        print(referring_word)
-        print(pronoun)
+        count = count + 1
+        sys.stdout = open("model_output.txt", "a")
+        phrase = row.p+" "+row.a1+" "+line
+        output = "Id: " +str(count)+ ", Sentence: "+ phrase+", Option1: "+referring_word+", Option2: " +referring_word+", Answer: "+referring_word  
+        print(output) 
+        #print(row.p+" "+row.a1+" "+line)
+        #print(referring_word)
+        #print(pronoun)
+        sys.stdout.close()
         # continue
     # break
-
