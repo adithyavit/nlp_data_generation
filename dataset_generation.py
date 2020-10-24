85e4c33f77318ef2ac6d7bcca18755e9fdcb42aa
adithya reddy <chreddy97@gmail.com>
2020-10-24 13:51:21 -0700

    using json format and options resolved now

--- dataset_generation.py
+++ dataset_generation.py
@@ -5,17 +5,18 @@ spacy.prefer_gpu()
 nlp = spacy.load("en_core_web_sm")
 import random
 import re
-
+from readAndWriteData import write_json
 data = pd.read_csv("balacopa-dev-all.csv")
 count = 0
+
 
 def select_replace(line, chunk):
     word = random.choice(tuple(chunk))
     pronoun = "it"
-    # line.replace(word, pronoun)
     line = re.sub(word, pronoun, line, flags=re.IGNORECASE)
     return line, word, pronoun
 
+output_dict = {}
 for row in data.itertuples(index=True, name='Pandas'):
     textp = row.p
     docp = nlp(textp)
@@ -31,48 +32,40 @@ for row in data.itertuples(index=True, name='Pandas'):
     doca2 = nlp(texta2)
     chunka2 = [chunk.text.lower() for chunk in doca2.noun_chunks]
     chunka2 = set(chunka2)
-    # print(chunkp)
-    # print(chunka1)
-    # print(chunka2)
 
     line = row.p+" "+row.a1+" "+row.a2
-    # print(len(chunkp.intersection(chunka1)))
-    # print(len(chunka1.intersection(chunka2)))
-    # print(len(chunkp.intersection(chunka2)))
-    if(len(chunkp.intersection(chunka1))>0):
-        line, referring_word, pronoun = select_replace(row.a1, chunka1)
-        sys.stdout = open("model_output.txt", "a")
-        count = count + 1
-        phrase = row.p+" "+line+" "+row.a2
-        output = "Id: " +str(count)+ ", Sentence: "+ phrase+", Option1: "+referring_word+", Option2: " +referring_word+", Answer: "+referring_word  
-        print(output)
-        #print(row.p+" "+line+" "+row.a2)
-        #print(referring_word)
-        #print(pronoun)
-        sys.stdout.close()
-        # continue
-    if(len(chunka1.intersection(chunka2))>0):
-        line, referring_word, pronoun = select_replace(row.a2, chunka2)
-        sys.stdout = open("model_output.txt", "a")
-        count = count + 1
-        phrase = row.p+" "+row.a1+" "+line 
-        output = "Id: " +str(count)+ ", Sentence: "+ phrase+", Option1: "+referring_word+", Option2: " +referring_word+", Answer: "+referring_word  
-        print(output)
-        #print(row.p+" "+row.a1+" "+line)
-        #print(referring_word)
-        #print(pronoun)
-        sys.stdout.close()
-        # continue
-    if(len(chunkp.intersection(chunka2))>0):
-        line, referring_word, pronoun = select_replace(row.a2, chunka2)
-        count = count + 1
-        sys.stdout = open("model_output.txt", "a")
-        phrase = row.p+" "+row.a1+" "+line
-        output = "Id: " +str(count)+ ", Sentence: "+ phrase+", Option1: "+referring_word+", Option2: " +referring_word+", Answer: "+referring_word  
-        print(output) 
-        #print(row.p+" "+row.a1+" "+line)
-        #print(referring_word)
-        #print(pronoun)
-        sys.stdout.close()
-        # continue
-    # break
+    intr1 = chunkp.intersection(chunka1)
+    intr2 = chunka1.intersection(chunka2)
+    intr3 = chunkp.intersection(chunka2)  
+
+    total_set = intr1.union(intr2, intr3)
+    total_length = len(total_set)
+
+    if (total_length ==2):
+        if(len(intr1)>0):
+            line, referring_word, pronoun = select_replace(row.a1, chunka1)
+            phrase = row.p+" "+line+" "+row.a2
+            ans = referring_word
+            option1 = total_set.pop()
+            option2 = total_set.pop()
+        elif(len(intr2)>0):
+            line, referring_word, pronoun = select_replace(row.a2, chunka2)
+            phrase = row.p+" "+row.a1+" "+line
+            ans = referring_word
+            option1 = total_set.pop()
+            option2 = total_set.pop()
+        elif(len(intr3)>0):
+            line, referring_word, pronoun = select_replace(row.a2, chunka2)
+            phrase = row.p+" "+row.a1+" "+line
+            ans = referring_word
+            option1 = total_set.pop()
+            option2 = total_set.pop()
+
+        count+=1
+        each_output = {}
+        each_output["Sentence"] = phrase
+        each_output["Option1"] = option1
+        each_output["Option2"] = option2
+        each_output["Answer"] = ans
+        output_dict[str(count)] = each_output
+    write_json(output_dict, "model_output.json")
